9480bc88cf0567eebc07b12c5746cef5c5b7b02c
Alessandro Tesi <tesi.alessandro88@gmail.com>
2026-01-19 02:10:22 +0100

    minor changes

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+from .network import NeuralNetwork
+from .layers import Layer
+from .trainer import Trainer
+from .losses import Loss
+from .utils import DataLoader
+from .activations import sigmoid
