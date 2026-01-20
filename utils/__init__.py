174fc9c770326bcbce48a2f2e074bd0724c7c640
Alessandro Tesi <tesi.alessandro88@gmail.com>
2026-01-20 19:30:14 +0100

    created model selection helpers

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,3 +2,4 @@ from .data_loader import DataLoader
 from .standard_scaler import StandardScaler
 from .load_monk import load_monk
 from .plot_curves import plot_curves
+from .model_selection_helpers import instability_coeff, tran_val_diff, count_parameters
---
+++ utils/model_selection_helpers.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+#helper function to count params that we will use later, since often we will get 100% acc, we chose the config with less parameters
+def count_parameters(architecture):
+    tot_params = 0
+    for i in range(len(architecture) - 1):
+        tot_params += architecture[i] * architecture[i+1] + architecture[i+1] # weights and bias
+    return tot_params
+
+# instability coefficient function
+def instability_coeff(val_losses: list) -> float:
+    """
+    Takes val loss vec and every time val loss increases we add the diff, uses relative diff, otherwise mse would be biased
+    """
+    instability = 0.0
+    for i in range(1, len(val_losses)):
+        if val_losses[i] > val_losses[i - 1]:
+            instability += (val_losses[i] - val_losses[i - 1]) / val_losses[i - 1]
+    return instability
+
+def tran_val_diff(train_losses: list, val_losses: list) -> float:
+    """
+    Computes the difference between train loss vec and val loss vec
+    """
+    return np.sum(abs(np.array(val_losses) - np.array(train_losses)))
