1b3c0131f591c408e05bfe2b20adde06cef9fbac
Alessandro Tesi <tesi.alessandro88@gmail.com>
2026-01-22 18:15:46 +0100

    min improvement = 0

--- model/trainer.py
+++ model/trainer.py
@@ -20,7 +20,7 @@ class Trainer:
                 y_val: np.ndarray = None,
                 epochs: int = 500,
                 early_stopping: int = 100,
-                min_improvement: float = 0.01,
+                min_improvement: float = 0.0,
                 eta: float = 0.01,
                 lam: float = 0.,
                 alpha: float = 0.,
